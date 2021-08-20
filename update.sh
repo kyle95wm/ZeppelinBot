cea98bb7cd40bcba0fd8c357aceb06ed306ae83a
Ben Richeson <benricheson101@gmail.com>
2021-08-19 20:30:44 -0400

    Fix update script

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Load nvm
-. ~/.nvm/nvm.sh
+# # Load nvm
+# . ~/.nvm/nvm.sh
+# 
+# # Run update
+# nvm use
+# git pull
+# npm ci
+# npm run build
+# pm2 restart process.json
 
-# Run update
-nvm use
-git pull
-npm ci
-npm run build
-pm2 restart process.json
+git fetch origin
+git reset --hard origin/bens-stuff
+docker-compose up --force-recreate --build -d
+docker image prune -f
